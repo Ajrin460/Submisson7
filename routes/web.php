a4c401c8aef7427c9e99a413831de9b22ec5f540
ajrinkari <karimajrin7707@gmail.com>
2023-12-23 16:13:35 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/about/{search}', function () {
+    $data = [
+        'pageTitle' => 'Tentang Kami',
+        'content' => 'Ini adalah halaman tentang kami .'
+    ];
+    return view('about', $data);
+});
+
+Route::get('/produk', 'App\Http\Controllers\ProdukController@index');
+
+route::get('profile',function){
+    $nama = "";
+    return view('profile, compact')
+
